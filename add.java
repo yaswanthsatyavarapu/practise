4b308718bb612abf2ceeb7e4dbc1979a84cac4cf
yaswanthsatyavarapu <89779321+yaswanthsatyavarapu@users.noreply.github.com>
2022-01-10 12:17:07 +0530

    final version

--- add.java
+++ add.java
@@ -8,5 +8,8 @@ public class Add {
     System.ot.println("sum of " + a + " and " + b + " is " + res);
     System.ot.println("Commited once ");
     System.ot.println("Commited twice ");
+    System.ot.println("Commited thrice ");
+    System.ot.println("Commited fourth");
+    System.ot.println("Commited fifth");
   }
 }
