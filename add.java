5ad9dc0cffd123d9cc78e832c6427a00c34e7c07
yaswanthsatyavarapu <89779321+yaswanthsatyavarapu@users.noreply.github.com>
2022-01-10 11:58:06 +0530

    add.java is created

---
+++ add.java
@@ -0,0 +1,10 @@
+
+
+public class Add {
+  public static void main(String[] args) {
+    int a = 8;
+    int b = 2;
+    int res = a + b;
+    System.ot.println("sum of " + a + " and " + b + " is " + res);
+  }
+}
